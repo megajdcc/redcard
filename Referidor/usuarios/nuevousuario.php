66a99dc03c366dfdf8cea825d918f3a115b32264
megajdcc <megajdcc2009@gmail.com>
2019-05-01 09:06:54 -0400

    Listar usuarios de forma general en franquiciatario y referidor 01/05/2019

--- Referidor/usuarios/nuevousuario.php
+++ Referidor/usuarios/nuevousuario.php
@@ -34,7 +34,7 @@ if(filter_input(INPUT_GET, 'ref')){
 }
 
 $includes = new Includes($con);
-$properties['title'] = 'Nuevo usuario referidor | eSmart Club';
+$properties['title'] = 'Nuevo usuario referidor | Travel Points';
 $properties['description'] = '';
 echo $header = $includes->get_no_indexing_header($properties);
 echo $navbar = $includes->get_admin_navbar(); ?>
@@ -43,7 +43,7 @@ echo $navbar = $includes->get_admin_navbar(); ?>
 					<div class="row">
 					
 						<?php echo $reg->getMethodError(); ?>
-						<div class="col-sm-12 col-sm-offset-4">
+						<div class="col-sm-12 col-sm-offset-2">
 							<div class="page-title">
 								<h1>Nuevo Usuario</h1>
 							</div><!-- /.page-title -->
